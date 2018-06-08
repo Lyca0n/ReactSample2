a4a57b905fb79a1dbb3e624eb515775b87057077
jvillalv <julio.villalva@autozone.com>
2018-06-08 16:19:53 -0600

    setup production conf webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,65 @@
 // outpun bundle 
 
 const path = require('path');
+const ExtractTextPlugin = require ('extract-text-webpack-plugin');
 
-module.exports ={
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const cssExtract = new ExtractTextPlugin('styles.css');
+
+    return {
         /**
          * Set main file to bundle 
          */
-    entry :"./src/app.js",
-    
-    output: {
-        /**
-         * Set out output file and name for the bundled javascript file
-         */
-        path:path.join(__dirname,'public'),
-        filename:'bundle.js'
-    },
-    mode:"development",
-    module:{
-        /**
-         * use babel-loader to run babel-core which replicate babel-cli functionality, to convert ES6 Syntax to ES5 as well as translating JSX into javascript
-         */
-        rules:[
-            {
-                loader:'babel-loader',
-                test: /\.js$/,
-                exclude : /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool:'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback:true
-      }
+        entry: "./src/app.js",
+
+        output: {
+            /**
+             * Set out output file and name for the bundled javascript file
+             */
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: "development",
+        module: {
+            /**
+             * use babel-loader to run babel-core which replicate babel-cli functionality, to convert ES6 Syntax to ES5 as well as translating JSX into javascript
+             */
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: cssExtract.extract({
+                        use:[
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap:true
+                                }
+                            },
+                            {
+                                loader:'sass-loader',
+                                options:{
+                                    sourceMap:true
+                                }
+                            }                            
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins:[
+            cssExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
