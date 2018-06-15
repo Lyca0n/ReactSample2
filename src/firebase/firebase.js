0baa303e47809ac23d65eb129244df6449edd59d
jvillalv <julio.villalva@autozone.com>
2018-06-15 15:04:29 -0600

    add all

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const config  = {
+    apiKey: "AIzaSyChptrE4q4p9Ml-jum434gPnipeRI_XW2g",
+    authDomain: "expensify-6fcdb.firebaseapp.com",
+    databaseURL: "https://expensify-6fcdb.firebaseio.com",
+    projectId: "expensify-6fcdb",
+    storageBucket: "expensify-6fcdb.appspot.com",
+    messagingSenderId: "756539405742"
+  };
+  firebase.initializeApp(config);
+
+  const databae = firebase.database();
+  database.ref()
+  .on('value', (snapshot)=>{
+    console.log(snapshot.val);
+  });
