5b03ce760a4fd09fd2533fbda2151884a5db720f
Julio Villalva <julio_cesar_villalva@hotmail.com>
2018-06-20 16:55:45 +0000

    store mock testing

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,10 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense,addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
     const action = removeExpense({ id: '123abc' });
@@ -29,4 +34,25 @@ test('should setup add expense action object with provided values ', () => {
             expense: expenses[1]
         }
     );
+});
+
+test('should add expense to database and store',(done)=>{
+    const store = createMockStore({});
+    const expenseData={
+        description: 'Mouse',
+        amount: 3000,
+        note:'this one is better',
+        createdAt: 1000
+    };
+    store.dispatch(startAddExpense(expenseData)).then(()=>{
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type:'ADD_EXPENSE',
+            expense:{
+                id:expect.any(String),
+                ...expenseData
+            }
+        });
+        done();
+    });
 });
