9223804286c4248a74cbda74f9093a3d41e46392
Julio Villalva <julio_cesar_villalva@hotmail.com>
2018-06-21 15:34:10 +0000

    add asych fetch testing

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense,addExpense, editExpense, removeExpense, setExpenses } from '../../actions/expenses';
+import { startAddExpense,addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -94,11 +94,24 @@ test('should add expense with defaults to database store',(done)=>{
         });    
 });
 
-test('should setup set expene action object with data', () => {
+test('should setup set expenes action object with data', () => {
     const action =setExpenses(expenses);
     expect(action).toEqual({
         type:'SET_EXPENSES',
         expenses
     });
    
+});
+
+test('should fetch the expenses from firebase', (done)=>{
+    const store = createMockStore({});
+    store.dispatch(startSetExpenses()).then(()=>{
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+           type:'SET_EXPENSES',
+           expenses
+        });
+        done();
+    });
+    
 });
