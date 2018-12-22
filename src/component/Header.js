276adb6e59ee7353434b95ea0c52706aa33d4412
Villalva <Julio.Villalva@autozone.com>
2018-12-21 18:35:43 -0700

    navigation style

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {startLogout} from '../actions/auth';
+import { startLogout } from '../actions/auth';
 
-export const Header = ({startLogout}) => (
-    <header>
-        <h1>Expensify</h1>
-        <ul>
-            <li>
-                <NavLink activeClassName="is-active" to="/dashboard">Dashboard</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="is-active" to="/create">Create Expenses</NavLink>
-            </li>
-            <li>
+export const Header = ({ startLogout }) => (
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard"><h1>Expensify</h1></Link>
                 <button onClick={startLogout}>Logout</button>
-            </li>
-        </ul>
+            </div>
+        </div>
     </header>
 );
 
-const mapDispatchToProps=(dispatch)=>({
-    startLogout: ()=> dispatch(startLogout())
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
 });
 
-export default connect(undefined,mapDispatchToProps)(Header);
+export default connect(undefined, mapDispatchToProps)(Header);
